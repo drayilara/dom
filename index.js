1805635306a810f1bd56738f11b7d3ac852af8b1
unknown <ayilarasodiq1@gmail.com git config --global user.name>
2022-05-21 23:33:15 +0100

    Learnt the origins of DOM

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const text = document.createTextNode("This is an image");
+const heading = document.createElement("h1");
+heading.appendChild(text);
+document.body.appendChild(heading);
+
+const image = document.createElement("img");
+const src = document.createAttribute("src");
+
+image.src = "https://cdn.shopify.com/s/files/1/0070/7032/files/AMgoal-setting_HEADER.jpg?v=1579623952&width=1024";
+
+document.body.appendChild(image);
+
+console.log(window.content);
