38d90b17bb3122e4913e257bc3949c89b247e6bb
unknown <ayilarasodiq1@gmail.com git config --global user.name>
2022-05-23 23:47:44 +0100

    Learnt how to use custom attributes

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+// Dynamically change input type upon clicking
 
-const menu = document.getElementById("menu");
+const availableTypes = ["radio", "number", "password", "month", "button", "checkbox", "date", "color"];
 
-const hr = document.createElement("hr");
 
-const isRepalced =  menu.firstElementChild;
+const input = document.querySelector("[type]");
 
-menu.replaceChild(hr, isRepalced);
+let index = 0;
+
+input.onclick = function(){
+    let currentType = availableTypes[index];
+    this.type = currentType;
+    ++index;
+
+    this.dataset.field = currentType;
+
+    if(index == (availableTypes.length-1)){
+        index = 0;
+    }
+
+    console.log(this.dataset.field);
+}
 
 
 
