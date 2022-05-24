88cfe049b2ea3d1a9033783062e9b2a9c9c33e39
unknown <ayilarasodiq1@gmail.com git config --global user.name>
2022-05-24 06:37:19 +0100

    Learnt about inline style methods

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-// Dynamically change input type upon clicking
-
-const availableTypes = ["radio", "number", "password", "month", "button", "checkbox", "date", "color"];
 
 
-const input = document.querySelector("[type]");
-
-let index = 0;
+const input = document.querySelector("input");
 
 input.onclick = function(){
-    let currentType = availableTypes[index];
-    this.type = currentType;
-    ++index;
-
-    this.dataset.field = currentType;
-
-    if(index == (availableTypes.length-1)){
-        index = 0;
-    }
-
-    console.log(this.dataset.field);
+    this.checked = true;
 }
 
+const p = document.querySelector("p");
 
+// p.style.cssText += "color:red; background-color: yellow; font-size: 100px;";
+// const inline = window.getComputedStyle(p);
+
+p.style.cssText += "background-color: pink; font-size: 100px; font-weight: 100px";
 
 
