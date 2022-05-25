15eca30fe566d82e7abafc8f29259102d75d38cb
unknown <ayilarasodiq1@gmail.com git config --global user.name>
2022-05-25 21:39:17 +0100

    mouse-events

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
 
-// collect them all
-const boxes = document.querySelectorAll(".child");
+const div = document.querySelector("div");
 const p = document.querySelector("p");
 
-// add event to all
+div.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+})
 
-boxes.forEach(box => {
-    box.onclick = function(){
-        // collect individual boxes
-        const topBox = document.querySelector(".top")
-        const middleBox = document.querySelector(".middle")
-        const bottomBox = document.querySelector(".bottom")
+div.addEventListener("mousedown", function(e){
+    let btn = e.button;
 
-        this.classList.add("borderBottom");
+    if(btn == 0){
+        p.textContent="You clicked with the left button";
+    } 
 
-        if(this.classList.contains("borderBottom")){
-            switch(this){
-                case topBox : middleBox.classList.remove("borderBottom");
-                              bottomBox.classList.remove("borderBottom");
-                              p.style["background-color"] = "green";
-                break;
-
-                case middleBox : topBox.classList.remove("borderBottom");
-                                 bottomBox.classList.remove("borderBottom");
-                                 p.style["background-color"] = "pink";
-                break;
-
-                case bottomBox : topBox.classList.remove("borderBottom");
-                                 middleBox.classList.remove("borderBottom");
-                                 p.style["background-color"] = "purple";
-                break;
-                     
-            }
-
-            // show the p
-            p.classList.add("visible");
-        }
-    }
+    if(btn == 2){
+        p.textContent="You clicked using the right button";
+    } 
 })
