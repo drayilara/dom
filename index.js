7e856bf1934bae0a037afaa6f80999c3ec1c8728
unknown <ayilarasodiq1@gmail.com git config --global user.name>
2022-05-25 18:55:42 +0100

    Understood load and unliad events,event propagation,bubbling and capturing,no sibling bubblibg

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 
-const input = document.querySelector("input");
-const logger = document.querySelector("p");
+const container = document.querySelector("#container")
 
-input.onchange = function(){
-    logger.textContent = this.value;
+// delegate event to children from parent
+
+container.addEventListener("click", addClicker)
+
+
+
+function addClicker(event){
+    // stop propagation
+        event.stopPropagation();
+        
+       const target = event.target
+       target.classList.toggle("borderBottom")
+
+    //    check if target has borderBottom before displaying p
+        const hasBlueBorder = target.classList.contains("borderBottom");
+
+    // grab p
+
+        const p = document.querySelector("p");
+
+        if(hasBlueBorder) p.classList.add("visible");
+        else p.classList.remove("visible");
 }
-
-
-
-
-
