a6a6217b001b6f6053044a73aaf2ee7b16ef10a4
unknown <ayilarasodiq1@gmail.com git config --global user.name>
2022-05-26 15:58:07 +0100

    Learnt about the scroll events and focus/blur events

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
+const password = document.querySelector("[type='password']");
+const text = document.querySelector("[type='text']");
 
-const div = document.querySelector("div");
-const logger = document.querySelector("p");
-
-div.ondblclick = function(e){
-        const cx = e.clientX;
-        const cy = e.clientY;
-        const sx = e.screenX;
-        const sy = e.clientY;
-
-        logger.textContent = `ClientX/Y: ${cx}/${cy}
-                             ScreenX/Y: ${sx}/${sy}`;
+password.onfocus = function(){
+        this.style["background-color"] = "yellow";
 }
 
-
+password.onblur = function(){
+        this.style["background-color"] = "pink";
+}
